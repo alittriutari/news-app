18ee294b8ec8fdcb061f554312eafdf230670b7c
alittriutari <alit.appkey@gmail.com>
2022-07-22 11:31:14 +0800

    update kotlin version

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:news_app/common/styles.dart';
 import 'package:news_app/data/model/article.dart';
 import 'package:news_app/ui/article_webview.dart';
 import 'package:news_app/ui/detail_page.dart';
 import 'package:news_app/ui/home_page.dart';
-import 'package:news_app/common/styles.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,16 +17,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'News App',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(elevation: 0),
         textTheme: myTextTheme,
         elevatedButtonTheme: ElevatedButtonThemeData(
             style: ElevatedButton.styleFrom(
-                primary: secondaryColor,
-                onPrimary: Colors.white,
-                textStyle: const TextStyle(),
-                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))))),
+                primary: secondaryColor, onPrimary: Colors.white, textStyle: const TextStyle(), shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))))),
         colorScheme: Theme.of(context).colorScheme.copyWith(primary: primaryColor, secondary: secondaryColor, onPrimary: Colors.white),
         bottomNavigationBarTheme: const BottomNavigationBarThemeData(
           selectedItemColor: secondaryColor,
@@ -41,13 +39,8 @@ class MyApp extends StatelessWidget {
               url: ModalRoute.of(context)?.settings.arguments as String,
             ),
       },
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
-      ],
-      supportedLocales: [Locale('id', ''), Locale('en', '')],
+      localizationsDelegates: const [AppLocalizations.delegate, GlobalMaterialLocalizations.delegate, GlobalWidgetsLocalizations.delegate, GlobalCupertinoLocalizations.delegate],
+      supportedLocales: const [Locale('id', ''), Locale('en', '')],
     );
   }
 }
